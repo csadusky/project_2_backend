7f8f7c8bc84a26144fc6393df0240664a08af675
courtneysadusky <courtneysadusky@gmail.com>
2015-05-31 21:01:52 -0400

    added Line modeland seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+Line.delete_all
+
+line = Line.create!(color: 'red', direction: 'inbound')
+line = Line.create!(color: 'red', direction: 'outbound')
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
